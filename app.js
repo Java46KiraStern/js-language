15125b766a94dc8c418795beb4da2de74b311f16
Kira <dr.kirastern@gmail.com>
2022-03-02 20:53:43 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log("Hello World");
+// for(var i = 0; i<3; i++){
+//     setTimeout(function(){
+//         console.log(i);
+//     })
+// }
+// for(let i = 0; i<3; i++){
+//     setTimeout(function(){
+//         console.log(i);
+//     })
+// }
+const fun = function(a, b){
+    return a + b;
+}
+let a = 5;
+// console.log(a ** 2);
+// console.log(fun(10, 20));
+//console.log(a(10, 20)); error
+//console.log(fun ** 2); NaN
+console.log("12" + 12); //1212
+console.log("12" - 12); //0
+console.log("ab" - 12); //NaN
+console.log(`+ "12" + 12 = ${ + "12" + 12}`); //+ "12" + 12 = 24
